57b8eacf12d889bfeb456be352431d91b6a26d23
Bhagabat Tripathy <bhagabat.tripathy20@gmail.com>
2025-05-17 12:52:18 +0530

    Updated HTML, CSS, and JS files with new layout

--- bhagu.js
+++ bhagu.js
@@ -1,4 +1,3 @@
- HEAD
 function showSection(id) {
   document.querySelectorAll("main section").forEach(section => {
     section.classList.add("hidden");
@@ -10,18 +9,3 @@ function showSection(id) {
 window.onload = () => {
   showSection('profile');
 };
-
-function showSection(sectionId) {
-    const sections = document.querySelectorAll("main section");
-    sections.forEach(section => {
-      section.classList.add("hidden");
-    });
-  
-    document.getElementById(sectionId).classList.remove("hidden");
-  }
-  
-  // Default view
-  document.addEventListener("DOMContentLoaded", () => {
-    showSection("profile");
-  });
- 
